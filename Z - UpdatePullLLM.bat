461239db58b2a85d4be6cb162f8a786017abc7a3
CtrlAiDel <40178706+CtrlAiDel@users.noreply.github.com>
2025-01-02 21:30:13 -0500

    Update Z - UpdatePullLLM.bat

--- Z - UpdatePullLLM.bat
+++ Z - UpdatePullLLM.bat
@@ -3,7 +3,7 @@
 color 36
 
 
-docker exec -it ollama /bin/sh -c "ollama list | awk -F: 'NR>1 && !/reviewer/ {system(\"ollama pull \"$1)}'"
+docker exec -it b569c998bf01 /bin/sh -c "ollama list | awk -F: 'NR>1 && !/reviewer/ {system(\"ollama pull \"$1)}'"
 
 
 pause
